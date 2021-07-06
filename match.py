c404fd3d99802dd0d8464cb90ef619090a6fe312
Caronte <caronte@pop-os.localdomain>
2021-07-06 17:28:42 -0500

    adding refresh

--- match.py
+++ match.py
@@ -1,13 +1,24 @@
 from generador import init
 from display import mostrar
 
-def turno(tablero):
-    a = input()
-    b = input()
+def turno(tablero, n):
+
+    print("\x1b[2J\x1b[H",end="")
+
+    mostrar(tablero, n)
+    print()
+    a = input("Ingrese Fila: ")
+    b = input("Ingrese Columna: ")
+    n = input("Ingrese Valor: ")
+    print()
+
+def is_fin(tablero):
+    return False
 
 def nivel(nivel):
     n = 3 if nivel == "dificil" else 2
     tablero = init(n)
-    mostrar(tablero, n)
+    while not is_fin(tablero):
+        turno(tablero, n)
 
 nivel("ez")
