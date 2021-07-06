6d45243d2ee7f60bcf4aa5d98b40feca14480ee2
Caronte <caronte@pop-os.localdomain>
2021-07-06 16:54:17 -0500

    adding display module

--- display.py
+++ display.py
@@ -1,8 +1,25 @@
+def separador(n, j):
+    return j != n*n - 1 and j % n == n - 1
+
+def format(n, j, sep, esp):
+    N = n ** 2
+    if separador(n,j):
+        print(esp+sep, end="")
+
+def printfila(fila, n, begin, sep, esp):
+    print(begin, end="   ")
+    N = n ** 2
+    for j in range(N):
+        if j: print(esp, end="")
+        print(fila[j], end="")
+        format(n, j, sep, esp)
+    print()
+
 def mostrar(matrix, n):
     N = n ** 2
+    printfila([chr(ord('A')+i) for i in range(N)],n," ", " ", " ")
+    print()
     for i in range(N):
-        for j in range(N):
-            print(matrix[i][j], end="")
-            if j and not (j+1) % n:
-                print("|", end="")
-        print()
+        printfila(matrix[i], n, i + 1, "|", ' ')
+        if separador(n, i):
+            printfila('-'*N,n, " ", "|", '-')
